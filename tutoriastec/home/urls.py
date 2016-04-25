b141d635bed9f0871f32ae0adee9d341c07fa72a
JonathanMarinFlores <blesto200@gmail.com>
2016-04-25 17:42:51 -0500

    Se modificó menu y anexo 6

--- tutoriastec/home/urls.py
+++ tutoriastec/home/urls.py
@@ -8,11 +8,10 @@ urlpatterns = [
     url(r'^$', views.inicio, name='inicio'),
     url(r'^home/$', views.home, name='home'),
     url(r'^login/$', views.login, name='login'),
-    url(r'^registro/$', views.registro, name='registro'),
+    url(r'^anexo6/$', views.registro, name='registro'),
     url(r'^recovery_passwd/$', views.recovery_passwd, name='recovery_passwd'),
     url(r'^log_out/$', views.log_out, name='log_out'),
     url(r'^alumno/perfil$', views.perfil, name='perfil'),
     url(r'^anexos/', TemplateView.as_view(template_name="anexos/anexos.html")),
     url(r'^contacto/', TemplateView.as_view(template_name="contacto.html")),
-    url(r'^anexo6/', TemplateView.as_view(template_name="anexos/anexo6.html")),
     ]
