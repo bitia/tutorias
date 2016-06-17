edaf9af13878941328fb21d3e94fbb6e6aa6e8a5
JonathanMarinFlores <blesto200@gmail.com>
2016-06-17 17:24:01 -0500

    cambios menores

--- tutoriastec/anexo8/urls.py
+++ tutoriastec/anexo8/urls.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url,patterns
 from .views import Parte1View, Parte2View
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    #   /anexo8/   
-    url(r'^parte1$', Parte1View.as_view() , name='anexo8'),
-    url(r'^parte2$', Parte2View.as_view() , name='anexo8'),
+    #   /anexo8/
+    url(r'^parte1$', Parte1View.as_view() , name='Parte1View'),
+    url(r'^parte2$', Parte2View.as_view() , name='Parte2View'),
+    url(r'^partes', TemplateView.as_view(template_name="anexo8/anexo8.html")),
    # url(r'^parte3$', Parte3View.as_view() , name='anexo8'),
 ]
