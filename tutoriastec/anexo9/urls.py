8d4780e9efdc822fd7356435a669331a40f19571
JonathanMarinFlores <blesto200@gmail.com>
2016-06-22 14:22:43 -0500

    Agregado anexo 9 Etapas

--- tutoriastec/anexo9/urls.py
+++ tutoriastec/anexo9/urls.py
@@ -4,5 +4,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     #   /anexo9/
-    url(r'^linea', TemplateView.as_view(template_name="anexo9/anexo9.html")),
+    url(r'^Linea_de_la_vida', TemplateView.as_view(template_name="anexo9/anexo9.html")),
+    url(r'^nuevo_suceso', TemplateView.as_view(template_name="anexo9/nuevo_suceso.html")),
+    url(r'^grafica', TemplateView.as_view(template_name="anexo9/grafica.html")),
+    url(r'^etapas', TemplateView.as_view(template_name="anexo9/etapas.html")),
 ]
